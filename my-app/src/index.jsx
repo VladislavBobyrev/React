c911f45d01399a2cc71ed3fe6e42ec9951a78a5c
vladislav <perets1995@gmail.com>
2021-04-17 21:10:30 +0300

    new app

--- my-app/src/index.jsx
+++ my-app/src/index.jsx
@@ -1,56 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './componients/app/'
 
-
-//const elements 
-//const elem = <h2>Hello World</h2>
-//const elem = React.createElement('h2', null, 'Hello World !!!')
-//const elem = (
-//	<div>
-//		<h2> Hello World !!!</h2>
-//		<input type='text' placeholder='Type here'/>		
-//	</div>
-//)
-
-// functions
-const Headers = () => {											//название переменной с заглавной 
-	return <h2>HELLO FUNCTION HEADER</h2>
-}
-const Inp = () => {
-    const hold = 'I,m holding Tesla'
-	return <input 
-                    type='text' 
-                    placeholder={hold}
-                    autoComplete = ""
-                    className = 'first'
-    />
-}
-const Btn = () => {
-    const styleField = {
-        width: '300px',
-        height: '300px',
-        background: 'black',
-         color: 'white',
-    }
-    //const text = 'Sign  up'
-    const res = () => {
-        return 'Login'
-    }
-	//return <button>{3 + 4}</button>
-	return <button
-        style = {styleField}
-    >{res()}</button>
-}
-
-const App = () => {		                                        //componient
-return (
-	<div>
-		<Headers/>
-		<Inp/>
-		<Btn/>
-	</div>	
-	)	
-}
-
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
