a7512b71b35c058ea43dd014493406e397fb1c15
vladislav <perets1995@gmail.com>
2021-04-22 21:57:04 +0300

    prototype eslint off

--- my-app/src/index.jsx
+++ my-app/src/index.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './componients/app/'
+//import App from './componients/app/'
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+const WhoAmI = ({ name, link }) => {
+	return (
+		<>
+			<h1> My name is {name}</h1>
+			<a href={link}>My Profile</a>
+		</>
+	)
+}
+ReactDOM.render(<WhoAmI name='Vladislav' link='https://github.com/VladislavBobyrev' />, document.getElementById('root'))
