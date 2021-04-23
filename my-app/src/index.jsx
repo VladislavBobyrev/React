9d381dcd90956e8d1f67dc436b67eb4bbb1d884d
vladislav <perets1995@gmail.com>
2021-04-23 18:21:29 +0300

    Componient React

--- my-app/src/index.jsx
+++ my-app/src/index.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 //import App from './componients/app/'
 
+class WhoAmI extends Component {
+	constructor(props){
+		super(props)
+	}
+	render(){
+		const { name, link } = this.props
+		return (
+			<>
+				<h1> My name is {name}</h1>
+				<a href={link}>My Profile</a>
+			</>
+		)
+	}
+}
+
 const WhoAmI = ({ name, link }) => {
-	return (
-		<>
-			<h1> My name is {name}</h1>
-			<a href={link}>My Profile</a>
-		</>
-	)
+
 }
 ReactDOM.render(<WhoAmI name='Vladislav' link='https://github.com/VladislavBobyrev' />, document.getElementById('root'))
