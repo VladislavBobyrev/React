f76c6498f79824579cffc972c09195af3839f44a
vladislav <perets1995@gmail.com>
2021-04-25 17:54:15 +0300

    scss style

--- list-deal/src/App.js
+++ list-deal/src/App.js
@@ -8,20 +8,20 @@ class App extends React.Component {
 
 		this.state = {
 			tasks: [
-				{id: 0, title: 'Create todo-react app', done: false},
-				{id: 1, title: 'Make video about it', done: true},
-				{id: 2, title: 'Create simple project JS', done: false}
+				{ id: 0, title: 'Create todo-react app', done: false },
+				{ id: 1, title: 'Make video about it', done: true },
+				{ id: 2, title: 'Create simple project JS', done: false }
 			]
 		}
 	}
 	render() {
 		const { tasks } = this.state
 
-		return(
+		return (
 			<div className='App'>
 				<h1 className='top'>Activ tasks: {tasks.length}</h1>
 				{tasks.map(task => (
-										<Task task={task} key={task.id}></Task>
+					<Task task={task} key={task.id}></Task>
 				))}
 			</div>
 		)
