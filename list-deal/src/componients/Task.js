ffdaf1a97cfdaad7bf549e3d98be2dbb4c2d8707
vladislav <perets1995@gmail.com>
2021-04-26 21:36:44 +0300

    bg

--- list-deal/src/componients/Task.js
+++ list-deal/src/componients/Task.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Task = ({ task, ...props }) => {
 
+	const ActivBtn = () => {
+	return	<div>{task.done ? <p>✔️</p> : <p>❌</p>}</div>
+	}
 	return (
 		<div className='task'>
 			<p>{task.title}</p>
+			<ActivBtn></ActivBtn>
 		</div>
 	)
 }
