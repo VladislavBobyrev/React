0540c390d7dd25b8d1ddc95353957e1852ce800a
vladislav <perets1995@gmail.com>
2021-04-29 13:10:36 +0300

    function value

--- list-deal/src/componients/WriteTasc/WriteTasc.js
+++ list-deal/src/componients/WriteTasc/WriteTasc.js
@@ -1,12 +1,21 @@
 import React from 'react'
 
+const activated = () => {
+	return (
+		console.dir('press button')
+	)
+}
+
 const WriteTask = () => {
 	return (
 		<div >
-			<input type='text'></input>
-			<button>NEW TASK</button>
+			<form>
+				<input type='text'></input>
+				<button onClick='activated()'>NEW TASK</button>
+			</form>
 		</div>
 	)
 }
+
 
 export default WriteTask
