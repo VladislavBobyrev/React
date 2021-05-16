ca49ebc19c0d5b7fe7206235fb7e0c75bba74d85
vladislav <perets1995@gmail.com>
2021-05-16 18:14:56 +0300

    new create react project

---
+++ lesson_react/my-app/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const header = (
+	<div>
+		<h2>hello </h2>
+		<input/>
+		<button/>
+	</div>
+)
+
+ReactDOM.render(header, document.getElementById('root'));
+
+
