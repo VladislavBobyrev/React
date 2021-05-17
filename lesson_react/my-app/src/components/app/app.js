6571ef1b2bab3de2821dc4fc8da72a2e31940687
vladislav <perets1995@gmail.com>
2021-05-17 16:17:45 +0300

    new create react project"

--- lesson_react/my-app/src/components/app/app.js
+++ lesson_react/my-app/src/components/app/app.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import AppHeader from '../app-header/app-header'
+import SearchPanel from '../search-panel/search-panel'
 
 const App = () => {
-	return(
-		<AppHeader/>
+	return (
+		<div>
+			<AppHeader />
+			<div className='search-panel d-flex'>
+				<SearchPanel />
+			</div>
+		</div>
 	)
 }
 export default App
---
+++ lesson_react/my-app/src/components/search-panel/search-panel.js
@@ -0,0 +1,11 @@
+import React from 'react'
+const SearchPanel  = () => {
+	return(
+		<input
+		className='form-control search-input'
+		type='text'
+		placeholder='Search panel'
+		/>
+	)
+}
+export default SearchPanel
