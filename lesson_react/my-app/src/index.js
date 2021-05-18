ab5ea7214b74fcd506726def042ff64faa48ea8a
vladislav <perets1995@gmail.com>
2021-05-18 15:19:35 +0300

    new create react project

--- lesson_react/my-app/src/index.js
+++ lesson_react/my-app/src/index.js
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import '../src/index.css'
 
 //import App from './components/app/app'
 
-function WhoImI (props) {
-	//<React.Fragment> = <>
-	return (		
+class WhoImI extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			years: 25, 
+			UsName: 'Vladislav VDV'
+		}
+	}
+
+	nextYears () {
+		console.log(1)
+		this.state.years++
+	}
+
+	render = () => {
+		const { name, surename, link } = this.props
+		return (
+			<>
+				<h1>My name is {this.state.UsName}, surename  is {surename} {this.state.years}</h1>
+				<a href={link}>My profil</a>
+			</>
+		)
+	}
+}
+
+
+const All = () => {
+	return (
 		<>
- 			<h1>My name is {props.name}, surename  is {props.surename}</h1>
-			<a href={props.link}>My profil</a>
+			<WhoImI name='Vladislav' surename='Bobyrev' link='google.com' />
+			<WhoImI name='Mary' surename='Bobyreva' link='google.com' />
+			<WhoImI name='Artem' surename='Bobyrev' link='google.com' />
 		</>
 	)
 }
 
-ReactDOM.render(<WhoImI name='Vladislav' surename='Bobyrev' link='google.com'/>, document.getElementById('root'));
+ReactDOM.render(<All />, document.getElementById('root'));
 
 
