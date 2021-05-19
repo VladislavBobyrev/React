91ff1915457be8d955a8eaf9b8d25e22ef1eca14
vladislav <perets1995@gmail.com>
2021-05-19 12:22:51 +0300

    createe bootstrap and reactstrap development

--- lesson_react/my-app/src/components/app-header/app-header.js
+++ lesson_react/my-app/src/components/app-header/app-header.js
@@ -1,16 +1,28 @@
 import React from 'react'
-import '../app-header/app-header.css'	
+import '../app-header/app-header.css'
 import styled from 'styled-components'
 
+// this is css in js style-componient fraemwork css
 const Header = styled.div`
-	display: flex;
+display: flex;
 	align-items: flex-end;
 	justify-content: space-beetwen; 
+	h1{
+		font-size: 26px;
+		color: ${props => props.colored ? 'red' : 'black'};
+		:hover{
+			color: blue;
+		}
+	}
+	h2{
+		font-size: 1.2rem;
+		color: grey;
+	}
 `
 
 const AppHeader = () => {
 	return (
-		<Header>
+		<Header colored>
 			<h1>Vladislav Bobyrev</h1>
 			<h2>5 записей из низ понравилось 0</h2>
 		</Header>
