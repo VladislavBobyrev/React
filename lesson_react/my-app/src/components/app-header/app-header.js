d74b69eca64263e26b7f9bca9599ca5e96c7b31a
vladislav <perets1995@gmail.com>
2021-05-19 12:11:47 +0300

    createe bootstrap and reactstrap development

--- lesson_react/my-app/src/components/app-header/app-header.js
+++ lesson_react/my-app/src/components/app-header/app-header.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import '../app-header/app-header.css'	
+import styled from 'styled-components'
+
+const Header = styled.div`
+	display: flex;
+	align-items: flex-end;
+	justify-content: space-beetwen; 
+`
 
 const AppHeader = () => {
 	return (
-		<div className='app-header d-flex'>
+		<Header>
 			<h1>Vladislav Bobyrev</h1>
 			<h2>5 записей из низ понравилось 0</h2>
-		</div>
+		</Header>
 	)
 }
 
