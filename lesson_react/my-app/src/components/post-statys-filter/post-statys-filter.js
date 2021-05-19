a3a24afd10bcb3e9b29cb9fc3fd144654b0eb49c
vladislav <perets1995@gmail.com>
2021-05-19 11:37:37 +0300

    createe bootstrap and reactstrap development

--- lesson_react/my-app/src/components/post-statys-filter/post-statys-filter.js
+++ lesson_react/my-app/src/components/post-statys-filter/post-statys-filter.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import '../post-statys-filter/post-status-filter.css'	
+import { Button } from 'reactstrap'
+import '../post-statys-filter/post-status-filter.css'
 
 const PostStatusFilter = () => {
-	return(
+	return (
 		<div className='btn-group'>
-			<button type='button' className='btn btn-info'>ВСЕ</button>
+			<Button outline color='info'>ВСЕ</Button>
 			<button type='button' className='btn btn-secondary'>Понравившиеся</button>
 		</div>
 	)
--- lesson_react/my-app/src/index.js
+++ lesson_react/my-app/src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css'
 import '../src/index.css'
 
 import App from './components/app/app'
